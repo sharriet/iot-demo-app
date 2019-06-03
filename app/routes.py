763c03b2f31f4833dc34099c8826ed1158c0c191
Sorrel Harriet <s.harriet@leedstrinity.ac.uk>
2019-06-03 17:22:46 +0100

    updating resource

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,31 @@
-from flask import Flask
+# import some functions and classes from external packages
+from flask import Flask, render_template
+import RPi.GPIO as GPIO
+import time
 
+# tell program what naming convention to use to identify I/O pins
+GPIO.setmode(GPIO.BCM)
+# suppress GPIO warninga
+GPIO.setwarnings(False)
+# tell Python that pin 18 will be used to output information
+GPIO.setup(18,GPIO.OUT)
+
+# initialuse the web app
 app = Flask(__name__)
 
+# define some 'routes' or 'views' in the app
 @app.route('/')
 def home():
-    return "Hello!"
+    return render_template('index.html', status='off')
 
+@app.route('/on')
+def on():
+    # Task 1b: put code here to turn light ON
+    GPIO.output(18,GPIO.HIGH)
+    return render_template('index.html', status='on')
+
+@app.route('/off')
+def off():
+    # Task 1b: put code here to turn light OFF
+    GPIO.output(18,GPIO.LOW)
+    return render_template('index.html', status='off')
